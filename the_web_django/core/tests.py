e8d8f622faebe3976239a1afed5db9a0c07a8c86
Jinnawat Makwisai <jinna@odds.team>
2021-06-08 22:26:11 +0700

    pytest

--- the_web_django/core/tests.py
+++ the_web_django/core/tests.py
@@ -1,3 +1,38 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from core.models import Profile, Subscriber
+
+class TestProfile(TestCase):
+  def test_profile_should_have_defined_fields(self):
+      profile = Profile.objects.create(
+        name="JinJohn",
+      )
+
+      assert profile.name == "JinJohn"
+
+
+class TestIndexView(TestCase):
+  def test_index_view_shoulds_see_Kan(self):
+    # Given
+    Profile.objects.create(name="Kan Ouivirach")
+
+    # When
+    response = self.client.get("/index-view/")
+    
+    # Then
+    assert response.status_code == 200
+    assert "Kan Ouivirach" in str(response.content)
+
+  def test_index_view_should_save_subscriber_email_when_input_form(self):
+    # Given
+    Profile.objects.create(name="Kan Ouivirach")
+
+    # When
+    data = {
+      "email": "kan@odds.team"
+    }
+    self.client.post("/index-view/", data=data)
+
+    # Then
+    subscriber = Subscriber.objects.last()
+    assert subscriber.email == "kan@odds.team"
